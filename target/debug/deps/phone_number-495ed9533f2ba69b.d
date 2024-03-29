2b3fd480744ac2f67f2b74fb7aa090d80cd0ad64
Lucas Barros <lcsavb@gmail.com>
2024-03-29 16:13:25 +0100

    not all test passing

---
+++ target/debug/deps/phone_number-495ed9533f2ba69b.d
@@ -0,0 +1,5 @@
+/home/lucas/dev/hi2/soft_dev/r03-01-phone-number-lcsavb/target/debug/deps/phone_number-495ed9533f2ba69b: tests/phone-number.rs
+
+/home/lucas/dev/hi2/soft_dev/r03-01-phone-number-lcsavb/target/debug/deps/phone_number-495ed9533f2ba69b.d: tests/phone-number.rs
+
+tests/phone-number.rs:
