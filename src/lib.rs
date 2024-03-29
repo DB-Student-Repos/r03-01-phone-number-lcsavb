5343ce94f6e3b32f6515ae3961db2df19a81d933
Lucas Barros <lcsavb@gmail.com>
2024-03-29 16:30:01 +0100

    all tests passing, yet ugly code

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,10 @@ pub fn number(user_number: &str) -> Option<String> {
             sanitized_number.push(c);
         }
     } 
+
+    if sanitized_number.chars().nth(0).unwrap() == '1' {
+        sanitized_number.remove(0);
+    }
     
     if sanitized_number.len() == 10 &&
        sanitized_number.chars().nth(0).unwrap() >= '2' &&
