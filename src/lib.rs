7471a12e01cdbaa383a481fa07a2444a660f51d5
Lucas Barros <lcsavb@gmail.com>
2024-03-29 20:41:05 +0100

    eliminating else

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,13 @@
 
-
 pub fn number(user_number: &str) -> Option<String> {
     let sanitized_number: String = user_number.chars().filter(|c| c.is_digit(10)).collect();
 
-
     match sanitized_number.len() {
         11 if sanitized_number.starts_with("1") => {
-            if check_nth_digits(&sanitized_number[1..]) {
-                Some(sanitized_number[1..].to_string())
-            } else {
-                None
-            }
+            Some(sanitized_number[1..].to_string()).filter(|s| check_nth_digits(s))
         }
         10 => {
-            if check_nth_digits(&sanitized_number) {
-                Some(sanitized_number)
-            } else {
-                None
-            }
+            Some(sanitized_number).filter(|s| check_nth_digits(s))
         }
         _ => None,
     }
@@ -26,3 +16,5 @@ pub fn number(user_number: &str) -> Option<String> {
 fn check_nth_digits(slice: &str) -> bool {
     slice.chars().nth(0).unwrap() >= '2' && slice.chars().nth(3).unwrap() >= '2'
 }
+
+
