009c6b21f06706f8b8ca39b7bdae7fbf638b6c87
Lucas Barros <lcsavb@gmail.com>
2024-03-29 20:32:44 +0100

    rewrite

--- src/lib.rs
+++ src/lib.rs
@@ -3,18 +3,26 @@
 pub fn number(user_number: &str) -> Option<String> {
     let sanitized_number: String = user_number.chars().filter(|c| c.is_digit(10)).collect();
 
-    match sanitized_number {
 
-        n if n.len() == 11 && n.starts_with('1') && 
-        n.chars().nth(1).unwrap() >= '2' && 
-        n.chars().nth(4).unwrap() >= '2' 
-        => Some(n[1..].to_string()),
-
-        n if n.len() == 10 && 
-        n.chars().nth(0).unwrap() >= '2' &&
-        n.chars().nth(3).unwrap() >= '2' => Some(n),
-
-        _ => None
+    match sanitized_number.len() {
+        11 if sanitized_number.starts_with("1") => {
+            if check_nth_digits(&sanitized_number[1..]) {
+                Some(sanitized_number[1..].to_string())
+            } else {
+                None
+            }
+        }
+        10 => {
+            if check_nth_digits(&sanitized_number) {
+                Some(sanitized_number)
+            } else {
+                None
+            }
+        }
+        _ => None,
     }
 }
 
+fn check_nth_digits(slice: &str) -> bool {
+    slice.chars().nth(0).unwrap() >= '2' && slice.chars().nth(3).unwrap() >= '2'
+}
